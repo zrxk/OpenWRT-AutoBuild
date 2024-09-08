30592101d9887547d86558c6f22a8bda79231257
zrxk <kimraid@gmail.com>
2024-09-08 15:36:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,8 +12,8 @@
 
 # Uncomment a feed source
 #sed -i '$a src-git smpackage https://github.com/kenzok78/small-package' feeds.conf.default
-sed -i '$a src-git zrxk https://github.com/zrxk/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '$a src-git zrxk https://github.com/zrxk/openwrt-packages' feeds.conf.default
+sed -i '$a src-git smallpacage https://github.com/kenzok8/small-package' feeds.conf.default
 
 # Add a feed source
 #echo 'src-git openwrt_kiddin9 https://github.com/kiddin9/openwrt-packages.git' >>feeds.conf.default
